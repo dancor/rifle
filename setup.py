7682a92d4e0e0eb39fc673f944d0680d859e6e7c
Daniel Corson <danl@alum.mit.edu>
2009-04-22 00:46:43 -0700

    initial template for rifle

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='rifle',
+    version='1.2',
+    description='<desc>',
+    author='<author>',
+    author_email='<email>',
+    url='<url>',
+    packages=['rifle'],
+    package_dir={'rifle': 'src'},
+    package_data={'rifle': []},
+    scripts=[],
+    data_files=[],
+    install_requires=[],
+)
