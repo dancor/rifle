22764cf9bb11e1f36afc3f5c7a9292a41bcf0758
Daniel Corson <danl@alum.mit.edu>
2009-04-22 15:31:48 -0700

    bogus version; and tkSnack does mp3(/wav/au/aiff/ogg)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rifle',
-    version='1.2',
+    version='0.0',
     description='interactively tag sound file segments',
     author='Chris Putnam',
     author_email='chrisputnam@gmail.com',
